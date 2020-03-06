a1d44ca361d34d5a0f1c9061d4c613ae4c6633da
inderjit <jeetinder60@ymail.com>
2020-03-06 09:35:45 -0800

    pythan scfript

--- my_square.py
+++ my_square.py
@@ -1,4 +1,12 @@
-def my_square(x):
-        return(x ** 2)
+def my_square(y):
+        """takes a value and return the squared value.
 
-print(my_square(3))
+           uses the *operator
+           """
+           return(y ** 2)
+
+def my_square2(x):
+           return(x * x)
+
+print(my_square(42))
+print(my_square2(42))
